654b6c07d2e167ee030bffd045b534a17b902c2d
Shubham Khandeparker <khandeparkershubhamoff01@gmail.com>
2025-10-18 20:20:46 +0530

    feat :Add drawable assets and display static dice image

--- app/src/main/java/com/shubham/app5diceroller/MainActivity.kt
+++ app/src/main/java/com/shubham/app5diceroller/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.shubham.app5diceroller
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.shubham.app5diceroller.ui.theme.App5DiceRollerTheme
 
@@ -21,8 +29,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceScreen(){
-    //Our UI and Logic will go here
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        //1.The Image Composable
+        Image(
+            painter= painterResource(id=R.drawable.dice_1),
+            contentDescription = "Dice showing a 1 "
+        )
+    }
 }
+
+fun column(function: () -> Unit) {}
 
 
 @Preview(showBackground = true)
